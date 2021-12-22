f8276c3bd500764bffb543b386018d88f5cbf824
richa <richa@DESKTOP-P3NFM11>
2021-12-22 15:00:06 +0800

    Add project files.

---
+++ FilemakerWebPortal/Controllers/AttendanceController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FilemakerWebPortal.Controllers
+{
+    public class AttendanceController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ FilemakerWebPortal/Services/ApiService.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace FilemakerWebPortal.Services
+{
+    public class ApiService
+    {
+        public string URL_BASE_AUTH = "https://209.141.7.130/fmi/data/v1/";
+        public string URL_POST = "https://209.141.7.130/fmi/data/v1/databases/Fabtech/layouts/FilemakerMobileAttendance/records";
+        public string URL_GET = "https://209.141.7.130/fmi/data/v1/databases/Fabtech/layouts/FilemakerMobileAttendance/records/";
+        public string URL_PATCH = "https://209.141.7.130/fmi/data/v1/databases/Fabtech/layouts/FilemakerMobileAttendance/records/";
+        public string URL_DELETE = "https://209.141.7.130/fmi/data/v1/databases/Fabtech/layouts/FilemakerMobileAttendance/records/";
+
+        public string? TOKEN;
+        public Dictionary<string, string> fieldData;
+
+        public string Database = "Fabtech";
+        public string Username = "root";
+        public string Password = "f@bt3ch";
+
+    }
+}
